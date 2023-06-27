9a7b484051e90b25abd0bfca2f9a27088a99ee0b
Nicholas Dille <nicholas.dille@mailbox.org>
2023-06-27 14:27:47 +0200

    Fixed output formatting

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -17,19 +17,19 @@ func main() {
 	fmt.Fprintf(os.Stderr, "                %032b\n", os.ModeCharDevice)
 	fmt.Fprintf(os.Stderr, "                %032b\n", os.ModeNamedPipe)
 	if fi.Mode()&os.ModeCharDevice == os.ModeCharDevice {
-		fmt.Fprintf(os.Stderr, "Stdin is a character device")
+		fmt.Fprintf(os.Stderr, "Stdin is a character device\n")
 	} else {
-		fmt.Fprintf(os.Stderr, "Stdin is not a character device")
+		fmt.Fprintf(os.Stderr, "Stdin is not a character device\n")
 	}
 	if fi.Mode()&os.ModeNamedPipe == os.ModeNamedPipe {
-		fmt.Fprintf(os.Stderr, "Stdin is a named pipe")
+		fmt.Fprintf(os.Stderr, "Stdin is a named pipe\n")
 	} else {
-		fmt.Fprintf(os.Stderr, "Stdin is not a named pipe")
+		fmt.Fprintf(os.Stderr, "Stdin is not a named pipe\n")
 	}
 	if isatty.IsTerminal(os.Stdin.Fd()) {
-		fmt.Fprintf(os.Stderr, "Stdin is a terminal")
+		fmt.Fprintf(os.Stderr, "Stdin is a terminal\n")
 	} else {
-		fmt.Fprintf(os.Stderr, "Stdin is not a terminal")
+		fmt.Fprintf(os.Stderr, "Stdin is not a terminal\n")
 	}
 
 	fo, err := os.Stdout.Stat()
@@ -38,21 +38,21 @@ func main() {
 		return
 	}
 	fmt.Fprintf(os.Stderr, "Mode for Stdout: %032b\n", fo.Mode())
-	fmt.Fprintf(os.Stderr, "                %032b\n", os.ModeCharDevice)
-	fmt.Fprintf(os.Stderr, "                %032b\n", os.ModeNamedPipe)
+	fmt.Fprintf(os.Stderr, "                 %032b\n", os.ModeCharDevice)
+	fmt.Fprintf(os.Stderr, "                 %032b\n", os.ModeNamedPipe)
 	if fo.Mode()&os.ModeCharDevice == os.ModeCharDevice {
-		fmt.Fprintf(os.Stderr, "Stdout is a character device")
+		fmt.Fprintf(os.Stderr, "Stdout is a character device\n")
 	} else {
-		fmt.Fprintf(os.Stderr, "Stdout is not a character device")
+		fmt.Fprintf(os.Stderr, "Stdout is not a character device\n")
 	}
 	if fo.Mode()&os.ModeNamedPipe == os.ModeNamedPipe {
-		fmt.Fprintf(os.Stderr, "Stdout is a named pipe")
+		fmt.Fprintf(os.Stderr, "Stdout is a named pipe\n")
 	} else {
-		fmt.Fprintf(os.Stderr, "Stdout is not a named pipe")
+		fmt.Fprintf(os.Stderr, "Stdout is not a named pipe\n")
 	}
 	if isatty.IsTerminal(os.Stdout.Fd()) {
-		fmt.Fprintf(os.Stderr, "Stdout is a terminal")
+		fmt.Fprintf(os.Stderr, "Stdout is a terminal\n")
 	} else {
-		fmt.Fprintf(os.Stderr, "Stdout is not a terminal")
+		fmt.Fprintf(os.Stderr, "Stdout is not a terminal\n")
 	}
 }
